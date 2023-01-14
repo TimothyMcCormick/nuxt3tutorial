8fdafdf15faffab1209a0eb8c486057693d502d7
Timothy McCormick <breaking490@gmail.com>
2023-01-14 12:14:54 -0700

    added route

--- pages/about.vue
+++ pages/about.vue
@@ -1,13 +1,21 @@
 <template>
-  
+  <div>
+    <h2>About</h2>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime itaque asperiores placeat laudantium veritatis consequatur ad illum exercitationem commodi vitae.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime itaque asperiores placeat laudantium veritatis consequatur ad illum exercitationem commodi vitae.</p>
+  </div>
 </template>
 
-<script>
-export default {
+<script setup>
 
-}
 </script>
 
-<style>
-
+<style scoped>
+h2 {
+  margin-bottom: 20px;
+  font-size: 36px;
+}
+  p {
+    margin: 20px 0;
+  }
 </style>
---
+++ pages/products/[id].vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <p>Product details for {{ id }}</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores praesentium culpa illo inventore laudantium voluptas suscipit qui repellendus deleniti sit.</p>
+  </div>
+</template>
+
+<script setup>
+  const { id } = useRoute().params
+</script>
+
+<style lang="scss" scoped>
+
+</style>
